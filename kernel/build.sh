nekoshirro/Alchemist-Toolchain

commit 843c760b30508c7a7a5bd418165483f36bee2b48

kernel/build.sh: Convert to LLVM=1
This is the standard for compiling with LLVM utilities now and it
potentially gains us more coverage with PGO. It is also a handy smoke
test in addition to the '--check-targets' flag.

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

kernel/build.sh
@@ -114,7 +114,7 @@ done
 
 # SC2191: The = here is literal. To assign by index, use ( [index]=value ) with no spaces. To keep as literal, quote it.
 # shellcheck disable=SC2191
-MAKE=(make -j"$(nproc)" -s CC=clang O=out)
+MAKE=(make -j"$(nproc)" -s O=out)
 
 header "Building kernels"
 
@@ -128,7 +128,7 @@ for TARGET in "${TARGETS[@]}"; do
                 ARCH=arm \
                 CROSS_COMPILE="${TARGET}-" \
                 KCONFIG_ALLCONFIG="${TC_BLD}"/kernel/le.config \
-                LD=ld.lld \
+                LLVM=1 \
                 distclean "${CONFIG_TARGET}" zImage modules || exit ${?}
             ;;
         "aarch64-linux-gnu")
@@ -137,7 +137,7 @@ for TARGET in "${TARGETS[@]}"; do
                 ARCH=arm64 \
                 CROSS_COMPILE="${TARGET}-" \
                 KCONFIG_ALLCONFIG="${TC_BLD}"/kernel/le.config \
-                LD=ld.lld \
+                LLVM=1 \
                 distclean "${CONFIG_TARGET}" Image.gz modules || exit ${?}
             ;;
         "powerpc-linux-gnu")
@@ -145,13 +145,16 @@ for TARGET in "${TARGETS[@]}"; do
                 "${MAKE[@]}" \
                 ARCH=powerpc \
                 CROSS_COMPILE="${TARGET}-" \
+                LLVM=1 \
                 distclean ppc44x_defconfig zImage modules || exit ${?}
             ;;
         "powerpc64-linux-gnu")
             time \
                 "${MAKE[@]}" \
                 ARCH=powerpc \
+                LD="${TARGET}-ld" \
                 CROSS_COMPILE="${TARGET}-" \
+                LLVM=1 \
                 distclean pseries_defconfig vmlinux modules || exit ${?}
             ;;
         "powerpc64le-linux-gnu")
@@ -159,6 +162,7 @@ for TARGET in "${TARGETS[@]}"; do
                 "${MAKE[@]}" \
                 ARCH=powerpc \
                 CROSS_COMPILE="${TARGET}-" \
+                LLVM=1 \
                 distclean powernv_defconfig zImage.epapr modules || exit ${?}
             ;;
         "s390x-linux-gnu")
@@ -166,12 +170,16 @@ for TARGET in "${TARGETS[@]}"; do
                 "${MAKE[@]}" \
                 ARCH=s390 \
                 CROSS_COMPILE="${TARGET}-" \
+                LD="${TARGET}-ld" \
+                LLVM=1 \
+                OBJCOPY="${TARGET}-objcopy" \
+                OBJDUMP="${TARGET}-objdump" \
                 distclean defconfig bzImage modules || exit ${?}
             ;;
         "x86_64-linux-gnu")
             time \
                 "${MAKE[@]}" \
-                LD=ld.lld \
+                LLVM=1 \
                 distclean "${CONFIG_TARGET}" bzImage modules || exit ${?}
             ;;
     esac

commit 84571b728d6e6a2690eecf43a85b8145b67495a7

kernel/build.sh: Account for host tools after LLVM=1
After the switch to LLVM=1, we implicitly assume that clang is being
built with host backend support. Unfortunately, this is not always the
case. Some users may chose to target LLVM only for backends they are
actively using, which does not have to include the host one. For
example,

$ ./build-llvm.py --pgo --targets "ARM;AArch64"

is common in the custom Android kernel scene because there are only ARM
chips. When kernel/build.sh is invoked by build-llvm.py when '--pgo' is
set, we will only build 'ARCH=arm' and 'ARCH=arm64' kernels. When we
were using 'CC=clang', there was no problem because clang was guaranteed
to have support for the target we were building. However, 'LLVM=1' not
only sets $(CC) to clang, it also sets $(HOSTCC) to clang, meaning that
the host tools such as scripts/dtc will be compiled with clang. If there
is no backend support for the host target (such a lack of x86_64 support
in the example above), we will get a rather cryptic message:

error: unable to create target: 'No available targets are compatible with triple "x86_64-unknown-linux-gnu"'
1 error generated.
make[3]: *** [scripts/Makefile.host:103: scripts/basic/fixdep] Error 1

To fix this, see if the host's target is in the list of supported
targets; if it is not, fall back to using GCC for HOSTCC and HOSTCXX.

Fixes: 843c760 ("kernel/build.sh: Convert to LLVM=1")
Reported-by: Panchajanya1999 <panchajanya@azure-dev.live>
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

kernel/build.sh
@@ -125,6 +125,16 @@ done
 # SC2191: The = here is literal. To assign by index, use ( [index]=value ) with no spaces. To keep as literal, quote it.
 # shellcheck disable=SC2191
 MAKE=(make -skj"$(nproc)" LLVM=1 O=out)
+case "$(uname -m)" in
+    arm*) [[ ${TARGETS[*]} =~ arm ]] || NEED_GCC=true ;;
+    aarch64) [[ ${TARGETS[*]} =~ aarch64 ]] || NEED_GCC=true ;;
+    mips*) [[ ${TARGETS[*]} =~ mips ]] || NEED_GCC=true ;;
+    ppc*) [[ ${TARGETS[*]} =~ powerpc ]] || NEED_GCC=true ;;
+    s390*) [[ ${TARGETS[*]} =~ s390 ]] || NEED_GCC=true ;;
+    riscv*) [[ ${TARGETS[*]} =~ riscv ]] || NEED_GCC=true ;;
+    i*86 | x86*) [[ ${TARGETS[*]} =~ x86_64 ]] || NEED_GCC=true ;;
+esac
+${NEED_GCC:=false} && MAKE+=(HOSTCC=gcc HOSTCXX=g++)
 
 header "Building kernels"
 